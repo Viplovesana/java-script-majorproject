3164e60a29b8a1934db6291b875cb75c887c1aa2
Viplovesana <viplovesana90@gmail.com>
2025-02-23 11:48:12 +0530

    new commit

---
+++ swiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // autoplay:{
+  //   delay:4000,
+  // }
+});
